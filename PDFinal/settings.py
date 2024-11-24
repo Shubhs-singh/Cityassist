1924d011837218eb14724af47613c4b43f6dcdff
Shubham Singh <145113243+Shubhs-singh@users.noreply.github.com>
2024-11-24 20:08:21 +0530

    removed secret key from setting.py

--- PDFinal/settings.py
+++ PDFinal/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8uvcgysrm!5-htffrjzqsf!%7vnh28rss&vamoh*x&$zx6#a4p'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
